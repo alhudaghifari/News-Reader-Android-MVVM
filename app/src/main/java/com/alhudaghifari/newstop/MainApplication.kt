76f010720c7efd7b95e4f7c03a92c10b2fec4b4c
alhudaghifari <alhudaghifari@gmail.com>
2021-09-13 23:26:11 +0700

    helper file added

---
+++ app/src/main/java/com/alhudaghifari/newstop/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.alhudaghifari.newstop
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()
---
+++ app/src/main/java/com/alhudaghifari/newstop/vo/Status.kt
@@ -0,0 +1,7 @@
+package com.alhudaghifari.newstop.vo
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
