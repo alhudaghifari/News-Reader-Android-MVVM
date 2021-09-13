a22522134cd2c23a627aca74bc00fa3787f9b457
alhudaghifari <alhudaghifari@gmail.com>
2021-09-13 20:28:18 +0700

    splash screen done

--- app/src/main/java/com/alhudaghifari/newstop/ui/main/MainActivity.kt
+++ app/src/main/java/com/alhudaghifari/newstop/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.alhudaghifari.newstop
+package com.alhudaghifari.newstop.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alhudaghifari.newstop.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/alhudaghifari/newstop/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.alhudaghifari.newstop.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
+import com.alhudaghifari.newstop.R
+import com.alhudaghifari.newstop.ui.main.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
